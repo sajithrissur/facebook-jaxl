07994b8424efb9ef03502b55b67ab3c257b0cacd
shadow-fox <pranaya.behera@outlook.com>
2013-06-13 17:40:07 +0530

    First Commit

---
+++ facebookChat.php
@@ -0,0 +1,89 @@
+<?php
+
+define('FACEBOOKBOT_PHP', TRUE);
+
+
+// Include XMPP Engine 'JAXL'
+// Path might be alter in time.
+// require_once ROOT_DIR.'/JAXL/jaxl.php';
+require_once 'jaxl.php';
+require_once 'xmpp/xmpp_msg.php';
+
+
+function sendMessage($client)
+{
+
+    $msg = new XMPPMsg(array('to'=>'617183653@chat.facebook.com'), 'test message');
+    $client->send($msg);
+
+    _info("test messages sent");
+}
+
+
+$user = '617183653'; 
+// $user = $argv[1];    // User name or facebook id
+$jidSuffix = '@chat.facebook.com';  // Facebook chat account suffix
+$appKey = '263501247121744';    // Taken from developer.facebook.com
+// $appKey = $argv[2];  // Facebook app token
+$accessToken = 'CAADvpyo7iVABAM9EZBToeyo0JrZCEBdY28KG4kcDCoG6efqhSdiugLJKEEEh56zAZCDzH2n2Bml1LxUtZBV1ZB676m4thwwQKZBBAzeS0z1no91QyR8BwlTq32Hp1wP9OGnhDom6w9qNA97U47mhMUOmGiAE6pzWEZD';  // Facebook user token - tried both app token tool on developer.facebook.com and token provided after user login both posses xmpp-login permission
+// $accessToken = $argv[3];
+
+$client = new JAXL( array(  // (required) credentials
+                            'jid' => $user.$jidSuffix,
+                            'fb_app_key' => $appKey,
+                            'fb_access_token' => $accessToken,
+
+                            // force tls (facebook require this now)
+                            'force_tls' => true,
+                            // (required) force facebook oauth
+                            'auth_type' => 'X-FACEBOOK-PLATFORM',
+
+                            // (optional)
+                            //'resource' => 'resource',
+
+                            'log_level' => JAXL_INFO,
+                            'priv_dir'  => '.'
+                        ));
+
+$client->add_cb('on_auth_success', function()
+{
+    global $client;
+    _info("got on_auth_success cb, jid ".$client->full_jid->to_string());
+    $client->set_status("available!", "dnd", 10);
+
+    // Here is the part where i tried to send message. In addition, i tried to call this function wherever i can on the code.
+    sendMessage($client);
+});
+
+$client->add_cb('on_auth_failure', function($reason)
+{
+    global $client;
+    $client->send_end_stream();
+    _info("got on_auth_failure cb with reason $reason");
+
+});
+
+$client->add_cb('on_chat_message', function($stanza)
+{
+    global $client;
+
+    // echo back incoming message stanza
+    $stanza->to = $stanza->from;
+    $stanza->from = $client->full_jid->to_string();
+    $client->send($stanza);
+
+    _info("echo message sent");
+
+    sendMessage($client);
+});
+
+$client->add_cb('on_disconnect', function()
+{
+    _info("got on_disconnect cb");
+});
+
+$client->start();
+
+echo "done\n";
+
+?>
